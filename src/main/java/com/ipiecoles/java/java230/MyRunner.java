d9a1c871b6e7c532cac96adccbe8efc5eafba006
pitchounvivi <pitchounvivi@gmail.com>
2020-11-03 15:05:46 +0100

    utilisation du repo avec Spring pour effectuer une recherche dans MyRunner.java

--- src/main/java/com/ipiecoles/java/java230/MyRunner.java
+++ src/main/java/com/ipiecoles/java/java230/MyRunner.java
@@ -1,16 +1,22 @@
 package com.ipiecoles.java.java230;
 
+import com.ipiecoles.java.java230.repository.EmployeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
 
+    @Autowired //pour pouvoir utiliser le repo (utilisation de Spring)
+    private EmployeRepository employeRepository;
+
     @Override
     public void run(String... strings) throws Exception {
         System.out.println("slt");
 
-
+        Long nbEmployes = employeRepository.count();
+        System.out.println("il y a " + nbEmployes + " dans la bdd");
 
 
     }
